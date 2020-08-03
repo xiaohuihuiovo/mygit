d4114bda3954dcc6516c5080b03df1a4746e9769
xiaohuihuiovo <1666352306@qq.com>
2020-08-03 17:26:39 +0800

    8.3

---
+++ calculator/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Compute></Compute>
+  </div>
+</template>
+
+<script>
+import Compute from './components/Compute.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Compute
+  }
+}
+</script>
+
+<style>
+
+</style>
